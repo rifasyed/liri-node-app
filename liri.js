f8254dac3f6f1c878ceba9427054011aa4cb24e5
Rifa Jafri <rifajafri@Rifas-MacBook-Pro.local>
2018-03-28 20:06:03 -0700

    added switch case

--- liri.js
+++ liri.js
@@ -1,10 +1,36 @@
 require("dotenv").config();
-var Twitter = require('twitter')
+var twitter = require('twitter')
 var spotify = require('spotify')
 var request = require('request')
 var fs = require('fs')
 var keys = require('./keys.js')
 
 var arg = process.argv[2]
-var search = process.argv[3]
+var itemSearch = process.argv[3]
 
+switch(command){
+    case "my-tweets":
+        twitterSearch();
+    break;
+    case "spotify-this-song":
+        spotifySearch()
+    break;
+    case "movie-this":
+        omdbSearch()
+    break;
+  
+    case "do-what-it-says":
+        doThing();
+    break;
+  
+    default:
+      console.log("{Please enter a command: my-tweets, spotify-this-song, movie-this, do-what-it-says}");
+    break;
+  }
+
+  function printSpotifySearch() {
+    console.log(d.tracks.items[i].name);
+    console.log(d.tracks.items[i].artists[0].name);
+    console.log(d.tracks.items[i].external_urls.spotify)
+    console.log(d.tracks.items[i].album.name)
+  }
